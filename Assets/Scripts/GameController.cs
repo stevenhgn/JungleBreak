c0c17f873058704780c56de27adbb893a3453dd1
Steven <steven.hgng@gmail.com>
2017-12-28 20:20:38 +0100

    add HUD

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public static GameController instance;
+    //public string[] iventory;
+    public List<string> iventory = new List<string>();
+    public int hp;
+
+    void Awake() {
+        // If we don't currently have a game controll....
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+   
+}
---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDScript : MonoBehaviour {
+    public Text items;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        string itemNames = "";
+
+        foreach(string itemName in GameController.instance.iventory)
+        {
+            itemNames += itemName + " ";
+
+        }
+
+        items.text = itemNames;
+            
+    }
+}
