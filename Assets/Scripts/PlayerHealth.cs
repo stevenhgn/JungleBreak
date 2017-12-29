7f1d0e2692ddc76ec0bbbf309ad1527db4ae0a01
Steven <steven.hgng@gmail.com>
2017-12-29 13:09:15 +0100

    Making Healthbar

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
+    public string HealthValue;
+
+    public Slider healthbar;
+
+
+	// Use this for initialization
+	void Start () {
+        MaxHealth = 100.0f;
+        CurrentHealth = MaxHealth;
+
+        healthbar.value = CalculateHealth();
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        HealthValue = CurrentHealth.ToString("R");
+
+        DealDamage(0.01f);
+
+        /*
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            DealDamage(6);
+        }
+        */
+	}
+
+    void DealDamage(float damageValue)
+    {
+        CurrentHealth -= damageValue;
+        healthbar.value = CalculateHealth();
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }  
+    }
+
+    float CalculateHealth() {
+        
+        return CurrentHealth / MaxHealth;
+    }
+
+    void Die() {
+        CurrentHealth = 0;
+        Debug.Log("You are dead.");
+    }
+}
