13b31e80ac1485217ebe6ab6d83ed04359d85785
Steven <steven.hgng@gmail.com>
2017-12-30 13:12:04 +0100

    added some textbox on scene1

---
+++ Assets/Scripts/PopUpWindow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpWindow : MonoBehaviour {
+    public GameObject window;
+    public Text dialougeText;
+
+	// Use this for initialization
+	public void Show(string message) {
+        dialougeText.text= message; 
+        //StopAllCoroutines();
+        //StartCoroutine(TypeSentence(dialougeText.text));
+
+        window.SetActive(true);
+		
+	}
+	
+	// Update is called once per frame
+	public void Hide () {
+        window.SetActive(false);
+	}
+
+    /*
+    IEnumerator TypeSentence (string sentence) {
+        dialougeText.text = " ";
+        foreach (char letter in sentence.ToCharArray()) {
+            dialougeText.text += letter;
+            yield return null;
+        }
+    }
+    */
+}
