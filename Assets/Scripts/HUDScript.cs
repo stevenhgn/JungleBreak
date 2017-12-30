cabff0fd36cfa866620783c09ab4a0c711f1faf5
Steven <steven.hgng@gmail.com>
2017-12-30 01:53:44 +0100

    end days button now works v2

--- Assets/Scripts/HUDScript.cs
+++ Assets/Scripts/HUDScript.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HUDScript : MonoBehaviour {
+public class HUDScript : MonoBehaviour
+{
     public Text items;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         string itemNames = "";
 
-        foreach(string itemName in GameController.instance.iventory)
+        foreach (string itemName in GameController.instance.iventory)
         {
             itemNames += itemName + " ";
 
         }
 
         items.text = itemNames;
-            
+
     }
 }
