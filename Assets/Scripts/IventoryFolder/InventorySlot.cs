314d543171034f928812735d8175e4470473c258
Steven <steven.hgng@gmail.com>
2018-01-12 13:53:50 +0100

    Implemented a working inventory system.

---
+++ Assets/Scripts/IventoryFolder/InventorySlot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour {
+
+    // Image variable icon
+    public Image icon;
+    // Button call removeButton
+    public Button removeButton;
+
+    ItemScript item;
+
+    // Add item methods that takes in one parameter Item from the item script
+
+    public void AddItem (ItemScript newItem) {
+        // local variable item equal newItem
+        item = newItem;
+
+        // sets the icon sprite to items icon
+        icon.sprite = item.icon;
+        // Sets the icon to be enable
+        icon.enabled = true;
+        // set the removeButton to be interactable
+        removeButton.interactable = true;
+    }
+
+    public void ClearSlot () {
+        item = null;
+
+        icon.sprite = null;
+        icon.enabled = false;
+        removeButton.interactable = false;
+    }
+
+    public void OnRemoveButton () {
+        Inventory.instance.Remove(item);
+    }
+
+    public void UseItem ()
+    {
+        if (item != null) {
+            item.Use();
+        }
+    }
+}
+  
--- Assets/Scripts/IventoryFolder/ItemScript.cs
+++ Assets/Scripts/IventoryFolder/ItemScript.cs
@@ -9,42 +9,12 @@ public class ItemScript : ScriptableObject {
     public float damageValue;
     public Sprite icon = null;
 
-    void Awake() {      
+    public virtual void Use () {
+        // use the item
+        //Something might happen
+
+        Debug.Log("Using " + name); 
     }
 
-    // Use this for initialization
-    void Start() {
-        // If the item was destroyed, it does not need to be recreated
-        /*
-        if (GameController.instance.itemsDestroyStatus.ContainsKey(itemID)){
-            if(GameController.instance.itemsDestroyStatus[itemID] == false){
-               Destroy(gameObject);
-            }
-        }*/
-    }
-    /*
-    void OnMouseDown() {
-        Destroy(gameObject);
-    
-        //tell the GameController-instance that the item is destroyed
-            GameController.instance.itemsDestroyStatus[itemID] = false; 
 
-        // heal or deal damage based on itemID - "H" for Health and "T" for Thirstiness
-        if(itemID.Substring(0, 1) == "H" && (GameController.instance.currentHp += damageValue) > GameController.instance.maxHp){
-            
-            //Add the item to iventory
-            GameController.instance.iventory.Add(itemName);
-            GameController.instance.currentHp = GameController.instance.maxHp;
-
-        }
-        // Same but for thirstiness
-        //int count = 0;
-        if (itemID.Substring(0, 1) == "T" && ((GameController.instance.currentThirst += damageValue) > GameController.instance.maxThirst)) {
-            GameController.instance.currentThirst = GameController.instance.maxThirst;
-            //Destroy(gameObject);
-            //count++;
-
-        }
-            
-    }*/
 }
