b3ee5eb67cb1bbee2880e65ef190e8951ca567ba
Steven <steven.hgng@gmail.com>
2018-01-14 20:48:55 +0100

    implemented player click on an item on the HUD to use it

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -2,12 +2,29 @@
 
 public class InventoryUI : MonoBehaviour {
 
+
+    // Not working entirely because of the fact that it only hold the information of the first time,.
+    //public static InventoryUI instance;
+
     public Transform itemsParent;
     public GameObject inventoryUI;
 
     Inventory inventory;
 
     InventorySlot[] slots;
+
+    /*
+    void Awake()
+    {
+        // If we don't currently have a game controll....
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(this); // this object needs to be kept throughout the wholegame
+    }*/
 
 	void Start () {
         inventory = Inventory.instance;
@@ -18,9 +35,9 @@ public class InventoryUI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
         if (Input.GetButtonDown("Inventory")) { 
             inventoryUI.SetActive (!inventoryUI.activeSelf);
+
         }
 	}
 
--- Assets/Scripts/Items/ItemScript.cs
+++ Assets/Scripts/Items/ItemScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 
-[CreateAssetMenu(fileName = "New Item", menuName = "Iventory/Item")]
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 
 public class ItemScript : ScriptableObject {
     new public string name = "New Item";
@@ -9,13 +9,27 @@ public class ItemScript : ScriptableObject {
     public float damageValue;
     public Sprite icon = null;
 
-    public virtual void Use () {
+    public virtual void Use()
+    {
         // use the item
         //Something might happen
+        Debug.Log("Using " + name);
+        // if currentHP pluss or minus the damagevalue is larger than maxHP then --> currentHP with be the MaxHP
+        // some kind of bugs making the damageValue to double which cause the currentHp to be wrong. 
+        //Temporary fix is to divide the damageValue with 2.
+        if (itemID.Substring(0, 1) == "H" && (GameController.instance.currentHp += damageValue/2) > GameController.instance.maxHp)
+        {
 
-        Debug.Log("Using " + name); 
+            //Add the item to iventory
+            //GameController.instance.iventory.Add(itemName);
+            GameController.instance.currentHp = GameController.instance.maxHp;
+            RemoveFromInventory();
+        }
+        else if(itemID.Substring(0, 1) == "H") {
+            GameController.instance.currentHp += damageValue/2;
+            RemoveFromInventory();
+        }
     }
-
     public void RemoveFromInventory () {
         Inventory.instance.Remove(this);
     }
